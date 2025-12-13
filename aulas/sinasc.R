73907fe9b5161ba5cbf357832ae0149b3a5372bd
Pedro Franklin <76104305+franklinpedro@users.noreply.github.com>
2025-12-12 21:02:08 -0300

    upload code sinasc.R

---
+++ aulas/sinasc.R
@@ -0,0 +1,8 @@
+library("remotes")
+library("tidyverse")
+library("data.table")
+library("microdatasus")
+
+remotes::install_github("danicat/read.dbc", force = T)
+
+remotes::install_github("rfsaldanha/microdatasus", force = TRUE)
